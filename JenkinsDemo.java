1222fd0830468a0486270d599301de159fd37d83
yash15031993 <71444268+yash15031993@users.noreply.github.com>
2020-09-17 18:34:46 +0530

    Update JenkinsDemo.java

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,5 +1,6 @@
 class JenkinsDemo{
 public static void main(String args[]){
-  System.out.println("My first jenkins");
+  System.out.println("Hurray!! you are done if your output is Finished: SUCCESS");
+  System.out.println("Finished: SUCCESS");
   }
 }
